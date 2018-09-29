741de3c0634db28b4a6b5eb5a838f85d4a30a9f2
Mitya Shabat <mitya.shabat@gmail.com>
2018-09-29 19:52:33 +0300

    Added the map

--- SpeedBumpMap/main.py
+++ SpeedBumpMap/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, make_response, json, request, Response
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, )
 
 @app.route('/')
 def mainMap():
     return render_template('mapPage.html')
+
+@app.route('/SpeedBumps')
+def speed_bumps():
+    return jsonify(eval(open('res/speedbumps.json', 'r').read()))
--- SpeedBumpMap/static/js/mapScript.js
+++ SpeedBumpMap/static/js/mapScript.js
@@ -1,12 +1,22 @@
-navigator.__defineGetter__('userAgent', function(){
-    return 'foo';
-});
-ymaps.ready( function() {
-    var map = new ymaps.Map(
-        "map",
-        {
-            center: [55.76, 37.64],
-            zoom: 10
-        }
-    );
-});
+
+function stretchMap() {
+    $("#mainMap").width($("html").width())
+    $("#mainMap").height($("html").height())
+}
+
+function setupMap() {
+    stretchMap();
+    $(window).resize(stretchMap);
+    ymaps.ready(function() {
+        var map = new ymaps.Map(
+            "mainMap",
+            {
+                center: [55.76, 37.64],
+                zoom: 10,
+                'options' : {
+                    'restrictMapArea' : [[55.924852, 37.275692], [55.572014, 37.938992]]
+                }
+            }
+        );
+    });
+}
