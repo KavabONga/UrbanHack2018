75c9e9763f16bdee8f9ccb09a733f9388a6b5417
Владимир Вислович <vislovichv@mail.ru>
2018-09-29 12:25:35 +0300

    added folders for templates

--- server/config/config/settings.py
+++ server/config/config/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,6 +68,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
