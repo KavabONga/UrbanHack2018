ea4ca1584ef93e1926b986ac3137c22fcf55afe2
Dmitrii Kocheshkov <dimakid123@gmail.com>
2018-09-30 15:41:53 +0300

    make_route changes

--- SpeedBumpMap/Scripts/make_route.py
+++ SpeedBumpMap/Scripts/make_route.py
@@ -2,19 +2,19 @@ import json
 import math
 
 
-def make_route(id):
-    def check(event, id):
+def make_route(bump_id):
+    def check(event, this_bump_id):
         for i in range(len(check_events)):
             if event['axles'] == check_events[i][0]['axles'] and \
                event['weight'] == check_events[i][0]['weight'] and\
                math.fabs(event['time'] - check_events[i][0]['time']) <= 1800:
-                event['id'] = id_dict[str(id)]
+                event['id'] = id_dict[str(this_bump_id)]
                 check_events[i].append(event)
-    inp_dict = open("res/IDs.json", "r").read()
-    inp_json = open("../SpeedBumpMap/res/carinfo-29-09.json", "r").read()
+    inp_dict = open("../res/IDs.json", "r").read()
+    inp_json = open("../res/carinfo-29-09.json", "r").read()
     carinfo = eval(inp_json)
     id_dict = eval(inp_dict)
-    check_bump_id = id
+    check_bump_id = bump_id
     check_bump_num = id_dict[str(check_bump_id)]
     check_events = list()
     for i in carinfo[check_bump_num]['events']:
@@ -28,14 +28,8 @@ def make_route(id):
     for i in range(check_bump_num + 1, len(carinfo)):
         for j in carinfo[i]['events']:
             check(j, carinfo[i]['ID'])
-    print(check_events)
     for car in check_events:
-        car.sort(key= lambda this_car: this_car['time'])
-    print(check_events)
-
-    ans_json = open("routes.json", "w")
-
-
+        car.sort(key=lambda this_car: this_car['time'])
     ans = list()
     for car in check_events:
         tmp_dict = dict()
@@ -48,6 +42,6 @@ def make_route(id):
             one_more_tmp_dict['id'] = i['id']
             tmp_dict['touches'].append(one_more_tmp_dict)
         ans.append(tmp_dict)
-    ans_json.write(json.dumps(ans))
+    return json.dumps(ans)
 
 make_route(24)
