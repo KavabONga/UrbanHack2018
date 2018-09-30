a908e07f2c7564f1e12351f78de60f4f96fe516a
Mitya Shabat <mitya.shabat@gmail.com>
2018-09-30 10:25:31 +0300

    Made two modes for the map

--- SpeedBumpMap/static/js/mapScript.js
+++ SpeedBumpMap/static/js/mapScript.js
@@ -4,19 +4,82 @@ function stretchMap() {
     $("#mainMap").height($("html").height())
 }
 
-function setupMap() {
-    stretchMap();
-    $(window).resize(stretchMap);
-    ymaps.ready(function() {
-        var map = new ymaps.Map(
-            "mainMap",
-            {
-                center: [55.76, 37.64],
-                zoom: 10,
-                'options' : {
-                    'restrictMapArea' : [[55.924852, 37.275692], [55.572014, 37.938992]]
-                }
+function MoscowMap(id) {
+    return new ymaps.Map(
+        id,
+        {
+            center: [55.76, 37.64],
+            zoom: 10,
+            controls: []
+        },
+        {
+            restrictMapArea : true
+        }
+    );
+}
+
+function addSpeedBumpView(map) {
+    $.get(
+        '/SpeedBumps',
+        function(data) {
+            bumpCollection = new ymaps.GeoObjectCollection();
+            for (speedbump in data) {
+                bumpCollection.add(new ymaps.Placemark(data[speedbump].coordinates, {
+                    balloonContent: data[speedbump].street
+                }, {
+                    preset: 'islands#circleIcon',
+                    openEmptyBaloon: true
+                }));
             }
-        );
+            map.geoObjects.add(bumpCollection);
+        }
+    )
+}
+
+function addCarCountView(map) {
+    $.get(
+        '/CarCount',
+        function(data) {
+            countCollection = new ymaps.GeoObjectCollection();
+            for (c in data) {
+                countCollection.add(
+                    new ymaps.Placemark(data[c].coordinates, {
+                        iconContent : data[c].count,
+                        balloonContent : "<b>Количество проехавших машин</b>: " + data[c].count + "<br><b>Суммарная масса</b>: " + data[c].summaryWeight + " (тонн)"
+                    }, {
+                        preset : "islands#redCircleIcon"
+                    })
+                )
+            }
+            map.geoObjects.add(countCollection);
+        }
+    )
+}
+
+function headerAlign() {
+    headerJ = $('#header');
+    headerJ.children().css('display', 'block');
+    headerJ.children().css('width', 100/headerJ.children().length + '%');
+}
+
+function switchView(map, view_func_add) {
+    return function() {
+        map.geoObjects.removeAll();
+        view_func_add(map);
+    }
+}
+
+function setupPage() {
+    stretchMap();
+    headerAlign();
+    $("#header").prop('hidden', false);
+    $(window).resize(stretchMap);
+    $(window).resize(headerAlign);
+    ymaps.ready(function() {
+        var map = MoscowMap("mainMap");
+        addSpeedBumpView(map);
+        $("#speedbump-button").click(switchView(map, addSpeedBumpView));
+        $("#carcount-button").click(switchView(map, addCarCountView));
+        window.map = map;
     });
 }
